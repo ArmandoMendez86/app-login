fb90b9eacb30fec22bc61a70bce244e988ac662b
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2024-11-04 21:12:17 -0600

    Corrigiendo ancho de formulario

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -21,9 +21,9 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-700">
-      <div className="space-y-6 bg-gray-800 text-white w-1/3 mx-auto p-8 rounded-lg shadow-lg mt-16 max-w-96">
-        <h2 className="text-2xl font-bold text-center">Iniciar Sesión</h2>
+    <div className="flex items-center justify-center min-h-screen bg-gray-700 px-4">
+      <div className="w-full max-w-md bg-gray-800 text-white p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center mb-6">Iniciar Sesión</h2>
         <form className="space-y-4" onSubmit={handleLogin}>
           <div className="flex flex-col">
             <label className="mb-2 text-sm font-medium">Usuario:</label>
@@ -46,7 +46,7 @@ const Login: React.FC = () => {
             />
           </div>
           <button
-            type="submit" // Cambié type a "submit" para manejar el evento correctamente
+            type="submit"
             className="w-full py-2 mt-4 bg-gray-600 hover:bg-gray-500 text-white font-semibold rounded transition duration-300"
           >
             Ingresar
